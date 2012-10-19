0b7195c1d21617b8237a5896644cf37e206df3c5
hanice <sunahnice@gmail.com>
2012-10-19 16:08:15 +0800

    crontab.20121019

--- 16sSNV/A2_quality_stat/4.dp.dist.py
+++ 16sSNV/A2_quality_stat/4.dp.dist.py
@@ -1,39 +1,53 @@
-from PyPlot.PyPlotClass import *
-
+import numpy as np
 def dist(xList,yList):
     s = 0
     for i in range(len(xList)):
         s += abs(xList[i] - yList[i])
     return s
         
-def dp_heatmap(inF,D):
-    inFile = open(inF)
-    dp = []
-    for line in inFile:
-        line = line.strip()
-        fields = line.split('\t')
-        x = []
-        y = []
-        for item in fields[-16:]:
-            d = int(item.split(':')[2])
-            if D == 0:
-                x.append(d)
-            else:
+def dp_dist(inF):
+    ds = []
+    for D in [0,3,5,10]:
+        inFile = open(inF)
+        dp = [[] for i in range(16)]
+        for line in inFile:
+            line = line.strip()
+            fields = line.split('\t')
+            for i,item in enumerate(fields[-16:]):
+                d = int(item.split(':')[2])
                 if d <=D:
-                    x.append(0)
+                    dp[i].append(0)
                 else:
-                    x.append(d)
-                    
+                    dp[i].append(1)
+                        
+        inFile.close()
+        tmp = []
         for i in range(8):
-            y.append(x[i])
-            y.append(x[i+8])
-        dp.append(y)
-    inFile.close()
-    return(dist(dp[i],dp[i+1]) [for i in range(8))])
+            tmp.append(dist(dp[i],dp[i+8]))
+        ds.append(tmp)
+    return ds
     
-dp_heatmap('sum_snv16s.exome_summary',0)
-dp_heatmap('sum_snv16s.exome_summary',3)
-dp_heatmap('sum_snv16s.exome_summary',5)
-dp_heatmap('sum_snv16s.genome_summary',0)
-dp_heatmap('sum_snv16s.genome_summary',3)
-dp_heatmap('sum_snv16s.genome_summary',5)
+    
+
+import matplotlib
+matplotlib.use('Agg')
+import numpy as np
+import matplotlib.pyplot as plt 
+
+sample = ['ICC4','ICC5','ICC9','ICC10','CHC5','CHC6','CHC7','CHC10']
+line = ['dp>=0','dp>=3','dp>=5','dp>=10']
+
+def plot(xList,ouFile):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(range(1,9),xList[0],range(1,9),xList[1],range(1,9),xList[2],range(1,9),xList[3])
+    ax.set_xticks(range(10))
+    ax.set_xticklabels(['']+sample+[''])
+    ax.legend(line,loc='upper left',bbox_to_anchor=[0.96,0.98],prop={'size':6})
+
+    plt.grid(True)
+    plt.savefig(ouFile)
+
+plot(dp_dist('sum_snv16s.exome_summary'), '1.depth.exome.dist.pdf')
+plot(dp_dist('sum_snv16s.genome_summary'), '1.depth.genome.dist.pdf')
+
