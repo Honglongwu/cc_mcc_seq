4155f74e792a523eb112255e359c3430917cc2ac
hanice <sunahnice@gmail.com>
2012-11-04 18:00:45 +0800

    crontab.20121104

--- viruses/CHC10B.sh
+++ viruses/CHC10B.sh
@@ -1,3 +1,27 @@
 cd /netshare1/home1/szzhongxin/proj1/hansun/viruses
 #samtools view /netshare1/home1/szzhongxin/proj1/hansun/mapping4/10B/10B.bam |python 1.filter.py >CHC10B.unmapped
-python 2.fq.py CHC10B.unmapped
+#python 2.fq.py CHC10B.unmapped
+
+read1fq=CHC10B.unmapped.fq
+read1sai=CHC10B.unmapped.sai
+bam=CHC10B.unmapped.bam
+
+cd /netshare1/home1/szzhongxin/proj1/hansun/viruses/mapping
+
+bwa aln -I \
+                /netshare1/home1/szzhongxin/proj1/hansun/viruses/bwa/viruses \
+                        /netshare1/home1/szzhongxin/proj1/hansun/viruses/$read1fq \
+                                >$read1sai
+
+bwa samse  \
+                /netshare1/home1/szzhongxin/proj1/hansun/viruses/bwa/viruses \
+                        $read1sai \
+                                /netshare1/home1/szzhongxin/proj1/hansun/viruses/$read1fq \
+                                        -r "@RG\tID:4a\tSM:4a\tLB:4a\tPL:illumina\tPU:barcode" | \ 
+        samtools view -bS -o $bam -
+
+        #samtools  sort $bam $bamsorted
+
+        #mv $bamsortedbam $BAM
+        #samtools index $BAM
+
--- viruses/ICC9A.sh
+++ viruses/ICC9A.sh
@@ -1,3 +1,27 @@
 cd /netshare1/home1/szzhongxin/proj1/hansun/viruses
 #samtools view /netshare1/home1/szzhongxin/proj1/hansun/mapping/9A/9A.bam |python 1.filter.py >ICC9A.unmapped
-python 2.fq.py ICC9A.unmapped
+#python 2.fq.py ICC9A.unmapped
+
+read1fq=ICC9A.unmapped.fq
+read1sai=ICC9A.unmapped.sai
+bam=ICC9A.unmapped.bam
+
+cd /netshare1/home1/szzhongxin/proj1/hansun/viruses/mapping
+
+bwa aln -I \
+	        /netshare1/home1/szzhongxin/proj1/hansun/viruses/bwa/viruses \
+		        /netshare1/home1/szzhongxin/proj1/hansun/viruses/$read1fq \
+			        >$read1sai
+
+bwa samse  \
+	        /netshare1/home1/szzhongxin/proj1/hansun/viruses/bwa/viruses \
+		        $read1sai \
+			        /netshare1/home1/szzhongxin/proj1/hansun/viruses/$read1fq \
+				        -r "@RG\tID:4a\tSM:4a\tLB:4a\tPL:illumina\tPU:barcode" | \ 
+        samtools view -bS -o $bam -
+
+	#samtools  sort $bam $bamsorted
+
+	#mv $bamsortedbam $BAM
+	#samtools index $BAM
+
