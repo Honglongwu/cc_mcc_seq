cf5a0bb6a23a1e4f1c7d182bd3afef3d5ebcb9fb
hanice <sunahnice@gmail.com>
2012-11-05 23:30:03 +0800

    crontab.20121105

--- viruses/blasted/1.num.py
+++ viruses/blasted/1.num.py
@@ -5,11 +5,11 @@ ouFile = open(sys.argv[1]+'.num','w')
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
-    D.setdefault(fields[1],0)
-    D[fields[1]] += 1
+    D.setdefault(fields[1],{})
+    D[fields[1]][fields[0]] = 1
 inFile.close()
 
 d = D.items()
-d.sort(cmp=lambda x,y:cmp(x[1],y[1]), reverse=True)
+d.sort(cmp=lambda x,y:cmp(len(x[1]),len(y[1])), reverse=True)
 for item in d : 
-    ouFile.write(item[0]+'\t'+str(item[1])+'\n')
+    ouFile.write(item[0]+'\t'+str(len(item[1]))+'\n')
