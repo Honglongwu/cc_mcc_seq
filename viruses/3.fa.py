03e4b765cb0c47c9e22f32c601370358e14c5274
hanice <sunahnice@gmail.com>
2012-11-05 19:00:15 +0800

    crontab.20121105

--- viruses/3.fa.py
+++ viruses/3.fa.py
@@ -4,8 +4,9 @@ ouFile = open(sys.argv[1]+'.fa', 'w')
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
-    ouFile.write('>'+fields[0]+'\n')
-    ouFile.write(fields[9]+'\n')
+    if fields[5] == '*':
+        ouFile.write('>'+fields[0]+'\n')
+        ouFile.write(fields[9]+'\n')
 inFile.close()
 ouFile.close()
 
