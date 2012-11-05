ec9abce1f315355e15aa1bfbf011688cf8e6dab1
hanice <sunahnice@gmail.com>
2012-11-05 20:00:08 +0800

    crontab.20121105

--- viruses/2.fq.py
+++ viruses/2.fq.py
@@ -4,11 +4,12 @@ ouFile = open(sys.argv[1]+'.fq', 'w')
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
-    ouFile.write('@'+fields[0]+'\n')
-    ouFile.write(fields[9]+'\n')
-    ouFile.write('+'+'\n')
+    if fields[5] =='*':
+        ouFile.write('@'+fields[0]+'\n')
+        ouFile.write(fields[9]+'\n')
+        ouFile.write('+'+'\n')
     #ouFile.write(fields[10]+'\n')
-    ouFile.write('h'*len(fields[9])+'\n')
+        ouFile.write('h'*len(fields[9])+'\n')
 inFile.close()
 ouFile.close()
 
