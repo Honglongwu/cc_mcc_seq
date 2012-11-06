937daf6ea5c259fba3cb84d8d0cad5992624bfe7
hanice <sunahnice@gmail.com>
2012-11-07 00:00:03 +0800

    crontab.20121107

--- viruses/blasted/4.heatmap.py
+++ viruses/blasted/4.heatmap.py
@@ -1,31 +1,35 @@
 from PyPlot.PyPlotClass import *
 import sys
-#def gene_heatmap(sampleNameList,ouF,figsize=0,rowList=[]):
-def gene_heatmap():
+
+MAXTICKS = 99
+def gene_heatmap(sampleNameList, ouF, figsize=0, rowList=[]):
     D = dict()
-    for i,inF in enumerate(sys.argv[1:]):
-        inFile = open(inF)
+    for i,inF in enumerate(sampleNameList):
+        inFile = open(inF + '.unmapped.sam.mapped.fa.fa.blasted.top.num')
         for line in inFile:
             line = line.strip()
             fields =line.split('\t')
             D.setdefault(fields[0],[0]*16)
-            D[fields[0]][i] = int(fields[1])
+            if int(fields[1]) < MAXTICKS:
+                D[fields[0]][i] = int(fields[1])
+            else:
+                D[fields[0]][i] = MAXTICKS
+
         inFile.close()
 
-    ouFile = open('xxx','w') 
     for k in D :
-        ouFile.write(k + '\t' + '\t'.join([str(x) for x in D[k]])+'\n')
-    '''
+        print(k)
+        print(D[k])
+        
     LD = []
     geneList = []
     for key in D :
         LD.append(D[key])
         geneList.append(key)
+    print(LD)
     
     pp=PyPlot(ouF)
     pp.heatmap(LD,col=False,xLabel=sampleNameList,yLabel=geneList,xLabelVertical=True,grid=True,figsize=figsize)
-    '''
 
-#gene_heatmap(['ICC1A','ICC2A','ICC3A','ICC6A','ICC7A','ICC8A','CHC1A','CHC2A','CHC3A','CHC4A','CHC8A','CHC9A'],'sv.exon.heatmap.pdf',figsize=(8,12))
+gene_heatmap(['ICC4A','ICC4B','ICC5A','ICC5B','ICC9A','ICC9B','ICC10A','ICC10B','CHC5A','CHC5B','CHC6A','CHC6B','CHC7A','CHC7B','CHC10A','CHC10B'],'viruses.heatmap.pdf',figsize=(8,12))
 
-gene_heatmap()
