dcbe986d314c7e1d966e83e276cc5930acf0f19e
hanice <sunahnice@gmail.com>
2012-11-06 22:30:03 +0800

    crontab.20121106

--- viruses/blasted/1.top.py
+++ viruses/blasted/1.top.py
@@ -5,14 +5,14 @@ ouFile = open(sys.argv[1]+'.top','w')
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
-    D.setdefault(fields[1],{})
-    if fields[0] not in D[fields[1]]:
-        D[fields[1]][fields[0]] = line
+    if fields[0] not in D:
+        D[fields[0]]=line
 inFile.close()
 
 d = D.items()
-d.sort(cmp=lambda x,y:cmp(len(x[1]),len(y[1])), reverse=True)
+d.sort(cmp=lambda x,y:cmp(x[1].split('\t')[1],y[1].split('\t')[1]))
 
-for item in d : 
-    for k in item[1]:
-        ouFile.write(item[1][k]+'\n')
+for item in d:
+    ouFile.write(item[1]+'\n')
+
+
