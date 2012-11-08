3b6bac95572819bd56182e68ec92fc52d63e4242
hanice <sunahnice@gmail.com>
2012-11-09 05:00:08 +0800

    crontab.20121109

---
+++ 16sSV/jumpy/4_blast/qsub.sh
@@ -0,0 +1,16 @@
+qsub 2.blastn.CHC10A.sh -l nodes=n4:ppn=8 -q high 
+qsub 2.blastn.CHC10B.sh -l nodes=n5:ppn=8 -q high 
+qsub 2.blastn.CHC5A.sh -l nodes=n6:ppn=8 -q high 
+qsub 2.blastn.CHC5B.sh -l nodes=n7:ppn=8 -q high 
+qsub 2.blastn.CHC6A.sh -l nodes=n9:ppn=8 -q high 
+qsub 2.blastn.CHC6B.sh -l nodes=n10:ppn=8 -q high 
+qsub 2.blastn.CHC7A.sh -l nodes=n11:ppn=8 -q high 
+qsub 2.blastn.CHC7B.sh -l nodes=n12:ppn=8 -q high 
+#qsub 2.blastn.ICC10A.sh -l nodes=n13:ppn=8 -q high 
+#qsub 2.blastn.ICC10B.sh -l nodes=n14:ppn=8 -q high 
+#qsub 2.blastn.ICC4A.sh -l nodes=n15:ppn=8 -q high 
+#qsub 2.blastn.ICC4B.sh -l nodes=n16:ppn=8 -q high 
+#qsub 2.blastn.ICC5A.sh -l nodes=n17:ppn=8 -q high 
+#qsub 2.blastn.ICC5B.sh -l nodes=n19:ppn=8 -q high 
+#qsub 2.blastn.ICC9A.sh -l nodes=n21:ppn=8 -q high 
+#qsub 2.blastn.ICC9B.sh -l nodes=n22:ppn=8 -q high 
