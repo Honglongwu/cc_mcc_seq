54f0265e1e9e3b3889c3140e29bb9e36c7b057ad
hanice <sunahnice@gmail.com>
2012-11-09 07:00:03 +0800

    crontab.20121109

---
+++ 16sSV/jumpy/4_blast/tmp.2.blastn.CHC10A.sh
@@ -0,0 +1,7 @@
+cd /netshare1/home1/szzhongxin/proj1/hansun/16sSV/jumpy/4_blast
+db=/netshare1/home1/people/hansun/Data/GenomeSeq/Human/ucsc.hg19.fasta
+
+query=tmp.CHC10A.jmp.fa
+out=tmp.${query}.blasted
+
+blastn -task megablast -db $db  -query $query -out $out -outfmt 6  -max_target_seqs 20    
