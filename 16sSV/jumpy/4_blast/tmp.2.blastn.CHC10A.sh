992b4214fea9c8e75b30811a63565139cae24387
hanice <sunahnice@gmail.com>
2012-11-09 08:00:03 +0800

    crontab.20121109

--- 16sSV/jumpy/4_blast/tmp.2.blastn.CHC10A.sh
+++ 16sSV/jumpy/4_blast/tmp.2.blastn.CHC10A.sh
@@ -4,4 +4,5 @@ db=/netshare1/home1/people/hansun/Data/GenomeSeq/Human/ucsc.hg19.fasta
 query=tmp.CHC10A.jmp.fa
 out=tmp.${query}.blasted
 
-blastn -db $db  -query $query -out $out -outfmt 6  -max_target_seqs 1
+#blastn -db $db  -query $query -out $out -outfmt 6  -max_target_seqs 1
+/netshare1/home1/people/hansun/MySoft/blast/ncbi-blast-2.2.27+/bin/blastn -db $db  -query $query -out $out -outfmt 6  -num_alignments 2
