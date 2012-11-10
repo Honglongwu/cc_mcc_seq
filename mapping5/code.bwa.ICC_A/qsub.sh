e3be235e61e769804374420c09eaa2e132f0da99
hanice <sunahnice@gmail.com>
2012-11-10 11:00:16 +0800

    crontab.20121110

--- mapping5/code.bwa.ICC_A/qsub.sh
+++ mapping5/code.bwa.ICC_A/qsub.sh
@@ -1,5 +1,3 @@
-cd /netshare1/home1/szzhongxin/proj1/hansun/Viruses/code.bwa.ICC_A
-
 qsub bwa10a.pbs -l nodes=n4:ppn=6 -q high
 #qsub bwa1ax.pbs -l nodes=n11:ppn=6
 #qsub bwa1ay.pbs -l nodes=n11:ppn=6
@@ -10,11 +8,8 @@ qsub bwa10a.pbs -l nodes=n4:ppn=6 -q high
 #qsub bwa3ax.pbs -l nodes=n11:ppn=6
 #qsub bwa3ay.pbs -l nodes=n11:ppn=6
 #qsub bwa3az.pbs -l nodes=n11:ppn=6
-qsub bwa4ax.pbs -l nodes=n5:ppn=6 -q high
-qsub bwa4ay.pbs -l nodes=n6:ppn=6 -q high
-qsub bwa4az.pbs -l nodes=n7:ppn=6 -q high
-qsub bwa5ax.pbs -l nodes=n9:ppn=6 -q high
-qsub bwa5ay.pbs -l nodes=n10:ppn=6 -q high
+qsub bwa4a.pbs -l nodes=n5:ppn=6 -q high
+qsub bwa5a.pbs -l nodes=n9:ppn=6 -q high
 #qsub bwa6ax.pbs -l nodes=n11:ppn=6
 #qsub bwa6ay.pbs -l nodes=n11:ppn=6
 #qsub bwa7ax.pbs -l nodes=n11:ppn=6
