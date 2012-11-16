71ff697cb5ae2b8254a7d99a1ba159d8e6d276ed
hanice <sunahnice@gmail.com>
2012-11-17 02:00:08 +0800

    crontab.20121117

--- Depth/Exome/1.depth.py
+++ Depth/Exome/1.depth.py
@@ -4,6 +4,7 @@ for i in range(3):
     line = sys.stdin.readline()
 
 depth = [0]*16
+depth2 = [0]*16
 site = [0]*16
 site2 = [0]*16
 
@@ -14,17 +15,22 @@ while True:
         fields = line.split('\t')
         for i,item in enumerate(fields[-16:]):
             d = int(item.split(':')[1])
-            if d >500:
-                depth[i]+=500
+            if d >800:
+                depth[i]+=800
             else:
                 depth[i]+=d
             if d > 0 :
                 site[i] += 1
-            if d > 50:
+            if d > 30:
                 site2[i] += 1
+                if d >800:
+                    depth2[i]+=800
+                else:
+                    depth2[i]+=d
     else:
         break
 
 print('\t'.join([str(x) for x in depth]))
 print('\t'.join([str(x) for x in site]))
+print('\t'.join([str(x) for x in depth2]))
 print('\t'.join([str(x) for x in site2]))
