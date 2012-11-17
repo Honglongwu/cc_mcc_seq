4284e776014178f55529893ca67e447be9e8eb8b
hanice <sunahnice@gmail.com>
2012-11-18 07:30:03 +0800

    crontab.20121118

--- 16sSV/jumpy/4_blat/3.blast.filter.py
+++ 16sSV/jumpy/4_blat/3.blast.filter.py
@@ -7,16 +7,49 @@ chrs = ['chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10',
         'chr11','chr12','chr13','chr14','chr15','chr16','chr17',
         'chr18','chr19','chr20','chr21','chr22','chrX','chrY','chrM']
 
+identity = 95
+size = 90
+if sys.argv[1].find('ICC4A') != -1:
+    size = 60
+
 D = {}
+def blat():
+    #inFile = open(sys.argv[1] + '.blat')
+    inFile = open('test')
+    for line in inFile:
+        line = line.strip()
+        fields = line.split('\t')
+        if float(fields[2]) >= identity and float(fields[3]) >= size:
+            D.setdefault(fields[0], [])
+            D[fields[0]].append(line)
+
+    inFile.close()
+
+blat()
+
+
 inFile = open(sys.argv[1])
-for line in inFile:
-    line = line.strip()
-    if line.find('>') == 0:
-        head = line
+while True:
+    line1 = inFile.readline().strip()
+    line2 = inFile.readline().strip()
+    line3 = inFile.readline().strip()
+    line4 = inFile.readline().strip()
+
+    if line1:
+        k1 = line1.lstrip('>')
+        k2 = line3.lstrip('>')
+
+        if k1 in D and k2 in D:
+            if len(D[k1]) == 1 and len(D[k2]) == 1:
+                print(D[k1][0])
+                print(line2)
+                print(D[k2][0])
+                print(line4)
+
     else:
-        D[head] = line
+        break
+
+
 
 inFile.close()
-
-print(len(D))
 
