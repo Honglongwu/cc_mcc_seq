76abf8a35897344de12a8985cacaf894a406c3fe
hanice <sunahnice@gmail.com>
2012-11-20 23:00:03 +0800

    crontab.20121120

--- 16sSNVExome/A4_split/4.tvew.py
+++ 16sSNVExome/A4_split/4.tvew.py
@@ -42,7 +42,7 @@ def readSNV():
 
 SNV=[]
 readSNV()
-ouFile = open('tview.v2.log','a')
+#ouFile = open('tview.v2.log','a')
 
 class Tview(threading.Thread):
     def __init__(self,snv,sample):
@@ -60,14 +60,18 @@ class Tview(threading.Thread):
             fcntl.ioctl(tty, termios.TIOCSTI,'\n')
             #screenshot(item,self.sample)
         except:
-            ouFile.write(item+':'+self.sample+'\n')
+            pass
+            #ouFile.write(item+':'+self.sample+'\n')
         #fcntl.ioctl(tty, termios.TIOCSTI,'q')
 
 
-samples = [['4A','mapping'],['4B','mapping3'],['5A','mapping'],['5B','mapping3'],
-        ['9A','mapping'],['9B','mapping3'],['10A','mapping'],['10B','mapping3'],
-        ['5A','mapping2'],['5B','mapping4'],['6A','mapping2'],['6B','mapping4'],
-        ['7A','mapping2'],['7B','mapping4'],['10A','mapping2'],['10B','mapping4']]
+samples = [['4A','mapping5'],['4B','mapping7'],['5A','mapping5'],['5B','mapping7'],
+        ['9A','mapping5'],['9B','mapping7'],['10A','mapping5'],['10B','mapping7'],
+        ['5A','mapping6'],['5B','mapping8'],['6A','mapping6'],['6B','mapping8'],
+        ['7A','mapping6'],['7B','mapping8'],['10A','mapping6'],['10B','mapping8']]
+
+samples = [['10A','mapping6'],['10B','mapping8']]
+
 
 for item in SNV:
     for s in samples:
@@ -75,4 +79,4 @@ for item in SNV:
         tv.start()
         os.system('samtools tview /netshare1/home1/szzhongxin/proj1/hansun/%s/%s/%s.bam /netshare1/home1/people/hansun/GATK/bundle/ucsc.hg19.fasta'%(s[1],s[0],s[0]))
     
-ouFile.close()
+#ouFile.close()
