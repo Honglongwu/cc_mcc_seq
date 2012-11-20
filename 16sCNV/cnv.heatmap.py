61bd932dfac4040f02d8a861d52fb4f5b535b958
hanice <sunahnice@gmail.com>
2012-11-20 09:00:03 +0800

    crontab.20121120

---
+++ 16sCNV/cnv.heatmap.py
@@ -0,0 +1,11 @@
+from PyPlot.PyPlotClass import *
+def gene_heatmap(sampleNameList,ouF,figsize=0,rowList=[]):
+    LD = []
+    
+    geneList = []
+    
+    pp=PyPlot(ouF)
+    pp.heatmap(LD,col=False,xLabel=sampleNameList,yLabel=geneList,xLabelVertical=True,grid=True)
+
+gene_heatmap(['ICC4A','ICC4B','ICC5A','ICC5B','ICC9A','ICC9B','ICC10A','ICC10B','CHC5A','CHC5B','CHC6A','CHC6B','CHC7A','CHC7B','CHC10A','CHC10B'],'sv.exon.heatmap.pdf')
+
