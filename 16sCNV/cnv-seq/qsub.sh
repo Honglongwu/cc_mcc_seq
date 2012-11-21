e96d7ab31212bd991ef9b6cb3894d54b56ecfccc
hanice <sunahnice@gmail.com>
2012-11-22 00:30:08 +0800

    crontab.20121122

---
+++ 16sCNV/cnv-seq/qsub.sh
@@ -0,0 +1,16 @@
+1_cnv-seq.CHC10A.sh
+1_cnv-seq.CHC10B.sh
+1_cnv-seq.CHC5A.sh
+1_cnv-seq.CHC5B.sh
+1_cnv-seq.CHC6A.sh
+1_cnv-seq.CHC6B.sh
+1_cnv-seq.CHC7A.sh
+1_cnv-seq.CHC7B.sh
+1_cnv-seq.ICC10A.sh
+1_cnv-seq.ICC10B.sh
+1_cnv-seq.ICC4A.sh
+1_cnv-seq.ICC4B.sh
+1_cnv-seq.ICC5A.sh
+1_cnv-seq.ICC5B.sh
+1_cnv-seq.ICC9A.sh
+1_cnv-seq.ICC9B.sh
