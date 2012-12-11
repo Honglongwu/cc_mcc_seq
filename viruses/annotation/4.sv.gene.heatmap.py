89995e29e0e0c98c97dbd65b8422ba6ecc477bc3
hanice <sunahnice@gmail.com>
2012-12-12 07:00:03 +0800

    crontab.20121212

--- viruses/annotation/4.sv.gene.heatmap.py
+++ viruses/annotation/4.sv.gene.heatmap.py
@@ -1,4 +1,12 @@
 from PyPlot.PyPlotClass import *
+
+
+'''
+pyHeatmap.py 156-165 changed to clolor viruses.
+'''
+
+
+
 def gene_heatmap(sampleNameList,ouF,figsize=0,rowList=[]):
     LD = []
     geneList = []
@@ -10,7 +18,7 @@ def gene_heatmap(sampleNameList,ouF,figsize=0,rowList=[]):
         geneList.append(fields[0])
     inFile.close()
     pp=PyPlot(ouF)
-    pp.heatmap(LD,col=False,xLabel=sampleNameList,yLabel=geneList,xLabelVertical=True,grid=True)
+    pp.heatmap(LD,col=False,xLabel=sampleNameList,yLabel=geneList,xLabelVertical=True,grid=True,figsize=[10,16])
 
 gene_heatmap(['ICC4A','ICC4B','ICC5A','ICC5B','ICC9A','ICC9B','ICC10A','ICC10B','CHC5A','CHC5B','CHC6A','CHC6B','CHC7A','CHC7B','CHC10A','CHC10B'],'viruse.pos.distribution.heatmap.pdf')
 
