f1daf5b40f3833973afaf3f9cfc9fb64d754e79e
hanice <sunahnice@gmail.com>
2012-12-12 08:30:03 +0800

    crontab.20121212

--- 16sCNV/cnv_gene/1.cnv-gene.py
+++ 16sCNV/cnv_gene/1.cnv-gene.py
@@ -8,6 +8,7 @@ for line in inFile:
 inFile.close()
 
 inFile = open('hg19_refGene.txt')
+ouFile = open(sys.argv[1]+'.depth', 'w')
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
@@ -17,16 +18,17 @@ for line in inFile:
     gene = fields[12]
     nm = fields[1]
     normal_dep = 0
-    normal_len = 0
+    #normal_len = 0
     tumor_dep = 0
-    tumor_len = 0
+    #tumor_len = 0
     for item in L:
         if item[0]==ch and start<=int(item[1])<=end and start<=int(item[2])<=end:
             normal_dep += float(item[4])
-            normal_len += int(item[3])
+            #normal_len += int(item[3])
             tumor_dep += float(item[5])
-            tumor_len += int(item[3])
-    if normal_len>0:
-        print(gene+'\t'+nm+'\t'+str(normal_dep)+'\t'+str(tumor_dep)+'\t'+str(normal_dep/normal_len)+'\t'+str(tumor_dep/tumor_len)+'\t'+str(tumor_dep/normal_dep))
+            #tumor_len += int(item[3])
+        #print(gene+'\t'+nm+'\t'+str(normal_dep)+'\t'+str(tumor_dep)+'\t'+str(normal_dep/normal_len)+'\t'+str(tumor_dep/tumor_len)+'\t'+str(tumor_dep/normal_dep))
+    ouFile.write(gene+'\t'+nm+'\t'+str(normal_dep)+'\t'+str(tumor_dep)+'\t'+str((tumor_dep+1)/(normal_dep+1))+'\n')
 inFile.close()
+ouFile.close()
 
