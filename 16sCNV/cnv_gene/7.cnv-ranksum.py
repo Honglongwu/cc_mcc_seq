2d2a8c8719033025ccd44ad1a55080b21c7c13db
hanice <sunahnice@gmail.com>
2012-12-13 19:00:03 +0800

    crontab.20121213

--- 16sCNV/cnv_gene/7.cnv-ranksum.py
+++ 16sCNV/cnv_gene/7.cnv-ranksum.py
@@ -3,8 +3,8 @@ def gene_two_group_ranksum_test(iFile,aList,bList) :
     ps=PyStats()
     ps.ranksum_test_file(iFile,aList,bList)
 
+gene_two_group_ranksum_test('6.varScan.copynumber.called.depth_2_upper_down_gene',[-8,-7,-6,-5],[-4,-3,-2,-1])
 gene_two_group_ranksum_test('6.varScan.copynumber.called.depth_1.5_upper_down_gene',[-8,-7,-6,-5],[-4,-3,-2,-1])
-#gene_two_group_ranksum_test('6.varScan.copynumber.called.depth_2_upper_down_gene',[-8,-7,-6,-5],[-4,-3,-2,-1])
 #gene_two_group_ranksum_test('5.varScan.copynumber.called.depth_1.5_upper_down_gene',[-8,-7,-6,-5],[-4,-3,-2,-1])
 #gene_two_group_ranksum_test('5.varScan.copynumber.called.depth_2_upper_down_gene',[-8,-7,-6,-5],[-4,-3,-2,-1])
 
