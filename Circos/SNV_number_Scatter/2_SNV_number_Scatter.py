7414f3e98a018aa250536c2d841c3d3dd278cbf8
hanice <sunahnice@gmail.com>
2012-12-26 01:00:46 +0800

    crontab.20121226

--- Circos/SNV_number_Scatter/2_SNV_number_Scatter.py
+++ Circos/SNV_number_Scatter/2_SNV_number_Scatter.py
@@ -12,9 +12,9 @@ def scatter_max(inFlist) :
 def scatter_format(inFlist) :
     m=scatter_max(inFlist)
     print(m)
-    m=4084.0
+    m=100
     for item in inFlist :
-        #m=scatter_max([item])
+        #m=not.repeat.scatter_max([item])
         inFile=open(item)
         ouFile=open(item+'tter','w')
         for line in inFile :
@@ -25,13 +25,13 @@ def scatter_format(inFlist) :
         ouFile.close()
 
 
-s=scatter_format(['SNV.genome.ICC4A.sca','SNV.genome.ICC4B.sca','SNV.genome.somatic.ICC4.sca'])
-s=scatter_format(['SNV.genome.ICC5A.sca','SNV.genome.ICC5B.sca','SNV.genome.somatic.ICC5.sca'])
-s=scatter_format(['SNV.genome.ICC9A.sca','SNV.genome.ICC9B.sca','SNV.genome.somatic.ICC9.sca'])
-s=scatter_format(['SNV.genome.ICC10A.sca','SNV.genome.ICC10B.sca','SNV.genome.somatic.ICC10.sca'])
-s=scatter_format(['SNV.genome.CHC5A.sca','SNV.genome.CHC5B.sca','SNV.genome.somatic.CHC5.sca'])
-s=scatter_format(['SNV.genome.CHC6A.sca','SNV.genome.CHC6B.sca','SNV.genome.somatic.CHC6.sca'])
-s=scatter_format(['SNV.genome.CHC7A.sca','SNV.genome.CHC7B.sca','SNV.genome.somatic.CHC7.sca'])
-s=scatter_format(['SNV.genome.CHC10A.sca','SNV.genome.CHC10B.sca','SNV.genome.somatic.CHC10.sca'])
+s=scatter_format(['SNV.genome.ICC4A.not.repeat.sca','SNV.genome.ICC4B.not.repeat.sca','SNV.genome.somatic.ICC4.not.repeat.sca'])
+s=scatter_format(['SNV.genome.ICC5A.not.repeat.sca','SNV.genome.ICC5B.not.repeat.sca','SNV.genome.somatic.ICC5.not.repeat.sca'])
+s=scatter_format(['SNV.genome.ICC9A.not.repeat.sca','SNV.genome.ICC9B.not.repeat.sca','SNV.genome.somatic.ICC9.not.repeat.sca'])
+s=scatter_format(['SNV.genome.ICC10A.not.repeat.sca','SNV.genome.ICC10B.not.repeat.sca','SNV.genome.somatic.ICC10.not.repeat.sca'])
+s=scatter_format(['SNV.genome.CHC5A.not.repeat.sca','SNV.genome.CHC5B.not.repeat.sca','SNV.genome.somatic.CHC5.not.repeat.sca'])
+s=scatter_format(['SNV.genome.CHC6A.not.repeat.sca','SNV.genome.CHC6B.not.repeat.sca','SNV.genome.somatic.CHC6.not.repeat.sca'])
+s=scatter_format(['SNV.genome.CHC7A.not.repeat.sca','SNV.genome.CHC7B.not.repeat.sca','SNV.genome.somatic.CHC7.not.repeat.sca'])
+s=scatter_format(['SNV.genome.CHC10A.not.repeat.sca','SNV.genome.CHC10B.not.repeat.sca','SNV.genome.somatic.CHC10.not.repeat.sca'])
 
 
