676e206dbbdcd64fd84f1e869925ebb8a49d4b26
hanice <sunahnice@gmail.com>
2013-01-21 21:30:03 +0800

    crontab.20130121

--- 16sSV/sv_all/6.sv.gene.heatmap.py
+++ 16sSV/sv_all/6.sv.gene.heatmap.py
@@ -1,4 +1,5 @@
 from PyPlot.PyPlotClass import *
+import re
 files = ['delly.1.sv.stat.gene.symbol','duppy.1.sv.stat.gene.symbol','invy.1.sv.stat.gene.symbol','jumpy.1.sv.stat.gene.symbol']
 def gene_heatmap(sampleNameList,ouF,figsize=0,rowList=[]):
     D = dict()
@@ -13,7 +14,7 @@ def gene_heatmap(sampleNameList,ouF,figsize=0,rowList=[]):
                 D.setdefault(item,[0]*16)
                 D[item][row]+=1
         inFile.close()
-    
+    ''' 
     ######
     D['MEA1']=D['MEA1/PTK7/CUL7/PPP2R5D/MRPL2/KLC4/KLHDC3/RRP36/PEX6']
     D['SLC25A21']=D['SLC25A21/LOC100129794/MIPOL1']
@@ -26,7 +27,21 @@ def gene_heatmap(sampleNameList,ouF,figsize=0,rowList=[]):
     D.pop('PARVB/PARVG/KIAA1644')
     D.pop('ALG3/MIR1224/EIF4G1/FAM131A/CLCN2/ECE2/PSMD2/SNORD66/CAMK2N2/VWA5B2')
     D.pop('ANXA1/ALDH1A1')
+    '''
+    Add = []
+    Del = []
+    for k in D:
+        fields = re.split(r'[/:]',k)
+        if len(fields)>1:
+            for item in fields:
+                if item!='*':
+                    Add.append([item,D[k]])
+            Del.append(k)
 
+    for x in Del:
+        D.pop(x)
+    for x in Add:
+        D[x[0]]=x[1]
 
     ouFile = open('16s.sv.exome.gene','w') 
     for k in D : 
