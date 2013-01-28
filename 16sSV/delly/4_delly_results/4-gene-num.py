40e0637dbeff9d8a4a2596b6d65d5b3e6a117a0f
hanice <sunahnice@gmail.com>
2013-01-28 09:30:03 +0800

    crontab.20130128

--- 16sSV/delly/4_delly_results/4-gene-num.py
+++ 16sSV/delly/4_delly_results/4-gene-num.py
@@ -6,16 +6,18 @@ for line in inFile:
     for item in fields[1:]:
         fds = item.split(':')
         if len(fds) ==4:
-            D[item]=1
+            D[':'.join(fds[0:-1])]=1
 inFile.close()
 print(len(D))
 
 inFile = open('1.sv.stat.gene.dbsnp2')
-D2 = {}
+D2={}
 for line in inFile:
     line = line.strip()
     if line.find('chr')==0:
-        D2[line]=1
+        k = ':'.join(line.split('\t'))
+        if k in D:
+            D2[k]=1
 inFile.close()
 print(len(D2))
 
