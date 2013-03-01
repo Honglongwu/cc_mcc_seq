afa547161fa534c7255b6b2123fc948e57ddb86f
hanice <sunahnice@gmail.com>
2013-03-02 00:00:30 +0800

    crontab.20130302

---
+++ 20sSV/duppy/4.sv.gene.heatmap.py
@@ -0,0 +1,25 @@
+from PyPlot.PyPlotClass import *
+def gene_heatmap(inF,sampleNameList,ouF,figsize=0,rowList=[]):
+    D = dict()
+    inFile = open(inF)
+    row = -1
+    for line in inFile:
+        row+=1
+        line = line.strip()
+        fields =line.split('\t')
+        for item in fields[1:]:
+            D.setdefault(item,[0]*20)
+            D[item][row]+=1
+    inFile.close()
+
+    LD = []
+    geneList = []
+    for key in D :
+        LD.append(D[key])
+        geneList.append(key)
+    
+    pp=PyPlot(ouF)
+    pp.heatmap(LD,col=False,xLabel=sampleNameList,yLabel=geneList,xLabelVertical=True,grid=True)
+
+gene_heatmap('1.sv.stat.gene.symbol', ['ICC1A','ICC2A','ICC3A','ICC4A','ICC5A','ICC6A','ICC7A','ICC8A','ICC9A','ICC10A','CHC1A','CHC2A','CHC3A','CHC4A','CHC5A','CHC6A','CHC7A','CHC8A','CHC9A','CHC10A'],'duppy.gene.heatmap.pdf')
+gene_heatmap('1.sv.stat.exon.symbol', ['ICC1A','ICC2A','ICC3A','ICC4A','ICC5A','ICC6A','ICC7A','ICC8A','ICC9A','ICC10A','CHC1A','CHC2A','CHC3A','CHC4A','CHC5A','CHC6A','CHC7A','CHC8A','CHC9A','CHC10A'],'duppy.exon.heatmap.pdf')
