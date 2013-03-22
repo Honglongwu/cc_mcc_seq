2195dc3166cf80b28e9c5f2fcbb46a02fbbe6606
hanice <sunahnice@gmail.com>
2013-03-23 01:00:04 +0800

    crontab.20130323

--- 20sViruses/6.heatmap.py
+++ 20sViruses/6.heatmap.py
@@ -12,9 +12,9 @@ V['NC_007605.1']='Human herpesvirus 4 type 1'
 V['NC_009334.1']='Human herpesvirus 4'
 V['NC_003977.1']='Hepatitis B virus'
 
-
-def foo(m):
-    if m >=8:
+Score = 20
+def foo(x):
+    if x >= Score:
         return 1
     else:
         return 0
@@ -51,7 +51,7 @@ def gene_heatmap(sampleNameList, ouF, figsize=0, rowList=[]):
     LD = []
     geneList = []
     for key in D3 :
-        if max(D3[key])>3:
+        if max(D3[key])>0:
             LD.append(D3[key])
             geneList.append(key)
     print(LD)
@@ -59,5 +59,5 @@ def gene_heatmap(sampleNameList, ouF, figsize=0, rowList=[]):
     pp=PyPlot(ouF)
     pp.heatmap(LD,col=False,xLabel=sampleNameList,yLabel=geneList,xLabelVertical=True,grid=True,figsize=figsize,colorTickets=True)
 
-gene_heatmap(['ICC1A','ICC2A','ICC3A','ICC4A','ICC5A','ICC6A','ICC7A','ICC8A','ICC9A','ICC10A','CHC1A','CHC2A','CHC3A','CHC4A','CHC5A','CHC6A','CHC7A','CHC8A','CHC9A','CHC10A'],'viruses.heatmap3.pdf',figsize=(6,8))
+gene_heatmap(['ICC1A','ICC2A','ICC3A','ICC4A','ICC5A','ICC6A','ICC7A','ICC8A','ICC9A','ICC10A','CHC1A','CHC2A','CHC3A','CHC4A','CHC5A','CHC6A','CHC7A','CHC8A','CHC9A','CHC10A'],'viruses.heatmap3.pdf')
 
